4d8777fc14baaf345afe124814b8107bca5ee066
Vladimir1mir <korneevdeath@yandex.ru>
2024-10-04 22:09:03 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,14 +1,16 @@
 from random import randint
 
 def rebus (n):
-    if 3 < n < 20:
+    if n < 3 or n > 20:
         return
     result = ''
     for i in range(1, n):
         for j in range(i + 1, n):
-            if (i + j) % n == 0:
+            if n % (i + j) == 0:
                 result = result + f'{i}' + f'{j}'
-    return result
+
+    return str(n) + ' - ' + str(result)
 
 count = randint(3,20)
 rebus(count)
+print(rebus(count))
